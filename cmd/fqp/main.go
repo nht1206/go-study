e240e600b7acb2003c00ae1e1a543faab8890e90
Nguyen Huu Tho <nghuutho74@gmail.com>
2023-08-31 22:50:41 +0700

    refactor(fqp): update the query in the example

--- cmd/fqp/main.go
+++ cmd/fqp/main.go
@@ -8,11 +8,24 @@ import (
 )
 
 func main() {
-	s := fqp.NewScanner(bytes.NewReader([]byte("(@collection.a == b && c == d) || a == c")))
+	scan("(a == 1 && b == 2) || c == 3")
+}
+
+func scan(query string) {
+	s := fqp.NewScanner(bytes.NewReader([]byte(query)))
 
 	for s.HasNext() {
 		t := s.Scan()
-
+		if t.Type == fqp.TokenGroup {
+			log.Println("group: ", t.Literal, "-----")
+			scan(t.Literal[1 : len(t.Literal)-1])
+			log.Println("endgroup------")
+			continue
+		}
 		log.Println(t.Type, t.Literal)
+	}
+
+	if s.Err != nil {
+		log.Println(s.Err)
 	}
 }
